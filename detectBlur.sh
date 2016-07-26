7155e8ca2faf94aaf4580c381dd70134920e88e2
christopherwmartin <cmarti14@artic.edu>
2016-07-26 11:01:24 -0500

    revised variables & added threshold observations

--- detectBlur.sh
+++ detectBlur.sh
@@ -7,6 +7,11 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 definedThreshold=0.004
+# The definedThreshold value may need to be tweaked depending on
+# the types of images that you are using.
+# Through my limited testing so far '0.004' has worked well for
+# bright photos taken in natural lighting while '0.006' has
+# given better results in more 'yellow', indoor lighting conditions.
 
 for file in *.jpg; do
 
@@ -22,7 +27,7 @@ for file in *.jpg; do
 
     percentage=$(echo "scale=4; ($white / $totalPixels)*100" | bc)
 
-    echo "$file edge -- $white/$totalPixels ($percentage%), Threshhold: $acceptableThreshold"
+    echo "$file edge -- $white/$totalPixels ($percentage%), threshold: $acceptableThreshold"
 
     if (("$white" < "$acceptableThreshold")) ;
         then
@@ -32,5 +37,5 @@ for file in *.jpg; do
 done
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm $DIR/*.gif
-#echo $DIR/*.gif
+echo $DIR/*.gif
+#rm $DIR/*.gif
